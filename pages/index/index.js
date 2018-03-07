cba5c6b40097df80e30af3fe1af1cc69f27f9a2d
kylezhao <517977572@qq.com>
2018-03-07 11:56:41 +0800

    对主页进行编辑，正在进行获取地址信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+var key = 'KSYBZ-S3HRD-OPO4I-HU6NO-J6CN2-ISBEU'//地图密钥key
 Page({
 
   /**
@@ -11,7 +12,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var lati,longi
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        lati = res.latitude.toString()
+        longi = res.longitude.toString()
+      }
+    })
+    console.log(lati)
+    var url = 'https://apis.map.qq.com/ws/geocoder/v1/?location=' + lati + ',' + longi + '&key='+key
+    console.log(url)
+    // wx.request({
+    //   url: url,
+    //   success:function(res){
+    //     console.log(res)  
+    //   }
+    // })
   },
 
   /**
