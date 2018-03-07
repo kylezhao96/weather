509f5ddefc5f03e240e0b7ae294d565406b5acaf
kylezhao <517977572@qq.com>
2018-03-07 16:03:59 +0800

    更改ui，api接口已经搞定

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,18 @@
-var key = 'KSYBZ-S3HRD-OPO4I-HU6NO-J6CN2-ISBEU'//地图密钥key
+var key = 'KSYBZ-S3HRD-OPO4I-HU6NO-J6CN2-ISBEU'//获取城市
+var hkey ='c6819ccf6dd348af8d5bc8664d92e818'//天气api
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    temperature:"4",
+    weather:"多云",
+    wind:"南风",
+    humidity:"69",
+    PM:"55",
     
+
   },
 
   /**
@@ -13,22 +20,47 @@ Page({
    */
   onLoad: function (options) {
     var lati,longi
+    var city,district
+    var that=this
+    //开始获取经纬度
     wx.getLocation({
       type: 'wgs84',
       success: function (res) {
         lati = res.latitude.toString()
         longi = res.longitude.toString()
+    //经纬度获取成功，开始转为位置   
+    wx.request({
+      url: 'https://apis.map.qq.com/ws/geocoder/v1/?location=' + lati + ',' + longi + '&key=' + key,
+      success:function(res){
+        console.log(res)
+        city = res.data.result.ad_info.city
+        district = res.data.result.ad_info.district
+        that.setData({
+          position: " "+city+" "+district
+        }) 
+        //位置获取成功，开始获取天气
+        wx.request({
+          url: "https://free-api.heweather.com/s6/weather?" +
+           "location="+city+"&" +
+          "key="+hkey+ "&" +
+          "lang=zh" + "&" +
+          "unit=m",
+          success:function(res){
+            that.setData({
+              temperature: res.data.HeWeather6[0].now.tmp,
+              weather: res.data.HeWeather6[0].now.cond_txt,
+              wind:res.data.HeWeather6["0"].now.wind_dir,
+              windnum:res.data.HeWeather6["0"].now.wind_sc,
+              humidity:res.data.HeWeather6["0"].now.hum,
+              feel: res.data.HeWeather6["0"].lifestyle["0"].brf
+            })
+            console.log(res)
+          }
+        })
       }
     })
-    console.log(lati)
-    var url = 'https://apis.map.qq.com/ws/geocoder/v1/?location=' + lati + ',' + longi + '&key='+key
-    console.log(url)
-    // wx.request({
-    //   url: url,
-    //   success:function(res){
-    //     console.log(res)  
-    //   }
-    // })
+      }
+    })
   },
 
   /**
